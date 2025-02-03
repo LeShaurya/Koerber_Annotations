73e01e1fc9830bb012f315e510729ce809bc6226
LeShaurya <raoljishauryadeep@gmail.com>
2025-02-03 15:41:52 +0530

    Lab 15

--- Lab15/book/src/main/java/com/example/ServletInitializer.java
+++ Lab15/book/src/main/java/com/example/ServletInitializer.java
@@ -1,4 +1,5 @@
-import com.example.DemoApplication;
+package com.example;
+
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
